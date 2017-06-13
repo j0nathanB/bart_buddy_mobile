f7fa29e02ec06cbc64b798061ccc880c503fae9f
Jackson Carter <jcarter0803@gmail.com>
2017-06-13 15:18:30 -0700

    still trying

--- src/train_view.js
+++ src/train_view.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View} from 'native-base';
 import MapView from 'react-native-maps';
 import styles from "./MapContainerStyles.js";
+import Destination from './train_info.js';
 
 const TrainView = ({station_coordinates, color}) => {
   	if ( color === "blue"){
@@ -9,28 +10,53 @@ const TrainView = ({station_coordinates, color}) => {
 	    <MapView.Marker 
 	      coordinate={station_coordinates}
 	      image={require('./train_images/blueTrain.png')}
-	    />
+	    >
+        <MapView.Callout >
+        <Destination Direction={"North"} 
+        Route={'Richmond/ PLasenton'} 
+       
+        time_to_next_station={'100 minutes'}  />
+        </MapView.Callout>
+	    </MapView.Marker>
 	  );
   	} else if ( color === "red" ) {
   	  return (
 	    <MapView.Marker 
 	      coordinate={station_coordinates}
 	      image={require('./train_images/redTrain.png')}
-	    />
+	    >
+	    <MapView.Callout >
+        <Destination Direction={"North"} 
+        Route={'Richmond/ PLasenton'} 
+        time_to_next_station={'100 minutes'}  />
+        </MapView.Callout>
+	    </MapView.Marker>
       );
   	} else if ( color === "yellow" ) {
       return (
 	    <MapView.Marker 
 	      coordinate={station_coordinates}
 	      image={require('./train_images/yellowTrain.png')}
-	    />
+	    >
+	    <MapView.Callout >
+        <Destination Direction={"North"} 
+        Route={'Richmond/ PLasenton'} 
+        time_to_next_station={'100 minutes'}  />
+        </MapView.Callout>
+	    </MapView.Marker>
      );
    } else if ( color === "orange" ) {
      return (
 	    <MapView.Marker 
 	      coordinate={station_coordinates}
-	      image={require('./train_images/orangeTrain.png')}
-	    />
+	      image={require('./train_images/redTrain.png')}
+	    >
+	    <MapView.Callout >
+        <Destination Direction={"North"} 
+        Route={'Richmond/ PLasenton'} 
+        time_to_next_station={'100 minutes'}  />
+        </MapView.Callout>
+	    </MapView.Marker>
      );
    }
 
