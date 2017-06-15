5dd6f543bfa225ad248b4c19c95445141f671847
Greg Coffeng <gcoffeng@gmail.com>
2017-06-15 16:31:00 -0700

    smaller screen area for BulletinList component

--- components/bulletinlist.js
+++ components/bulletinlist.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import { Container, Content, List, ListItem, Text } from 'native-base';
-export default class BulletinList extends Component {
+
+export default class Bulletin27bstroke6 extends Component {
   constructor(props) {
     super(props);
   }
+
     render() {
       return (
         <Container>
           <Content>
-            <List>
-              {this.props.schedule.filter( lineItem => lineItem.destination === this.props.route
-                ).map((cur, idx) => { 
-                  return (
-                  <ListItem key={idx}>
-                    <Text  key={idx}>Next {cur.destination} Train: {cur.minutes} minutes</Text>
-                  </ListItem>
-                  )
-              })}
-            </List>
+            {this.props.schedule.filter( lineItem => lineItem.destination === this.props.route
+                ).filter( lineItem => lineItem.minutes !== "Leaving"
+                ).map((cur, idx) => {
+                    return ( <Text key={idx}>Next {cur.destination} Train: {cur.minutes} minutes</Text> )
+                  })}
           </Content>
         </Container>
       );
     }
 }
+
+// if ((cur.minutes === "Leaving") || (cur.minutes === "0")) {
+//                     return ( <Text key={idx}>Next {cur.destination} Train: {cur.minutes} minutes</Text> )
+//                   } else {
+//                     return ( <Text key={idx}>Next {cur.destination} Train: {cur.minutes} minutes</Text> )
+//                   }})}
