02da6d5811bc27409c7a95b75ef24e0e64c717bf
Greg Coffeng <gcoffeng@gmail.com>
2017-06-15 17:52:34 -0700

    white background added to text-only bulletinlist

--- components/bulletinlist.js
+++ components/bulletinlist.js
@@ -1,7 +1,12 @@
-import React, { Component } from 'react';
-import { Container, Content, List, ListItem, Text } from 'native-base';
+import React, { 
+  Component,  
+  StyleSheet,
+  View,
+  Dimensions
+} from 'react';
+import { Container, Content, List, Card, CardItem, Text } from 'native-base';
 
-export default class Bulletin27bstroke6 extends Component {
+export default class Bulletinlist extends Component {
   constructor(props) {
     super(props);
   }
@@ -9,20 +14,34 @@ export default class Bulletin27bstroke6 extends Component {
     render() {
       return (
         <Container>
-          <Content>
-            {this.props.schedule.filter( lineItem => lineItem.destination === this.props.route
-                ).filter( lineItem => lineItem.minutes !== "Leaving"
-                ).map((cur, idx) => {
-                    return ( <Text key={idx}>Next {cur.destination} Train: {cur.minutes} minutes</Text> )
-                  })}
+        <Content>
+                {
+                  this.props.schedule.filter( lineItem => lineItem.destination === this.props.route)
+                  .map( (cur, idx) => { return ( 
+                      <Text style={{ backgroundColor: 'white'}} key={idx}>Next {cur.destination} Train: {cur.minutes} minutes</Text>
+                    )}
+                      )
+                }
           </Content>
         </Container>
       );
     }
 }
 
-// if ((cur.minutes === "Leaving") || (cur.minutes === "0")) {
-//                     return ( <Text key={idx}>Next {cur.destination} Train: {cur.minutes} minutes</Text> )
-//                   } else {
-//                     return ( <Text key={idx}>Next {cur.destination} Train: {cur.minutes} minutes</Text> )
-//                   }})}
+   // render() {
+   //    return (
+   //      <Container>
+   //        <Content>
+   //              {
+   //                this.props.schedule.filter( lineItem => lineItem.destination === this.props.route)
+   //                .map( (cur, idx) => { return ( 
+   //                  <View style={{flex: 1, backgroundColor: 'red'}}>
+   //                    <Text key={idx}>Next {cur.destination} Train: {cur.minutes} minutes</Text>
+   //                  </View> 
+   //                  )}
+   //                    )
+   //              }
+   //        </Content>
+   //      </Container>
+   //    );
+   //  }
